05f6085f3f60a774bdd6b84541a6115bade7bd3e
john githiyon <johngithiyon7@gmail.com>
2026-05-26 12:04:58 +0530

    feat: add the  func to display the command usage

---
+++ internal/services/commands/helpcmd.go
@@ -0,0 +1,16 @@
+package commands
+
+import "log"
+
+func Helpcmd() {
+
+	   log.Println("-----------------------------------------")
+	   log.Println("To Open Folder Use: openfolder foldername")
+	   log.Println("-----------------------------------------")
+	   log.Println("To Open File Use: openfile directorypath")
+	   log.Println("-----------------------------------------")
+	   log.Println("To Open Browser Use: openbrowser browsername")
+	   log.Println("------------------------------------------")
+	   log.Println("To Search In Google Use: google searchvalue")
+	   log.Println("-------------------------------------------")
+}
