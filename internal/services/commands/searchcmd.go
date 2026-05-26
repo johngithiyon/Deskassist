e061fcbc5d72430f38b4db7911a3c8a1b7c2c719
john githiyon <johngithiyon7@gmail.com>
2026-05-26 10:24:42 +0530

    feat: add the search command for user search

---
+++ internal/services/commands/searchcmd.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"log"
+	"os/exec"
+)
+
+
+func Searchcmd(arg string ) {
+
+	     log.Println("Search Commad")
+	 
+	     searchout,searcherr := exec.Command("xdg-open","https://google.com/search?q="+arg).Output()
+
+         if searcherr != nil {
+			log.Println(searcherr)
+			return 
+		 }
+
+		 log.Println(string(searchout))
+	     
+}
