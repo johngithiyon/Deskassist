72f5fe754998998fe10f80d4e3f1acb77fbd15d2
john githiyon <johngithiyon7@gmail.com>
2026-05-26 10:25:09 +0530

    feat: add the func to decode the cmd

---
+++ internal/services/decodecmd.go
@@ -0,0 +1,19 @@
+package services
+
+import "github.com/johngithiyon/Deskassist/internal/services/commands"
+
+
+func Decodecmd(cmd string,arg string) {
+   
+	    if cmd == "open" {
+		   
+			 commands.Open(arg)
+			   
+		} else if cmd == "google" {
+             
+			 commands.Searchcmd(arg)
+		 	
+		}
+        
+	     
+}
