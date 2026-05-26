3b1f9ac719bcae70751c2daa80766000d5644a80
john githiyon <johngithiyon7@gmail.com>
2026-05-26 11:37:01 +0530

    refactor: rename the filename

---
+++ internal/services/commands/openfolder.go
@@ -0,0 +1,50 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/johngithiyon/Deskassist/internal/errors"
+)
+
+
+func Openfolder(arg string ) {
+	 
+      cmd :=  exec.Command("find","/home/john", "-type","d", "-name", arg)
+	 
+	  output,runerr := cmd.Output()
+
+	  if runerr != nil {
+		   log.Println(runerr)
+	  }
+
+	  log.Println(string(output))
+
+	  var folderpath string 
+	  
+	  log.Println("Enter folder path")
+	  
+	  lines,scanerr := fmt.Scanf("%s",&folderpath)
+
+	  if lines != 1 {
+		    log.Println(errors.ErrInputlines)
+			return 
+	  }
+
+	  if scanerr != nil {
+		     log.Println(scanerr)
+			 return 
+	  }
+
+	 _,openerr :=  exec.Command("xdg-open",folderpath).Output()
+
+
+	 if openerr != nil {
+		  
+		    log.Println(openerr)
+			return 
+	 }
+
+
+}
